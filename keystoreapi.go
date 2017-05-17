e515d0160823e9addeed64bbb4c8a412ea93c18f
Simon Tucker <brotherlogic@gmail.com>
2017-05-16 20:40:50 -0700

    First pass at goserver implementation. This closes #9.

--- keystoreapi.go
+++ keystoreapi.go
@@ -1,7 +1,47 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+
+	"google.golang.org/grpc"
+
+	"github.com/brotherlogic/goserver"
+	pb "github.com/brotherlogic/keystore/proto"
+)
+
+// DoRegister does RPC registration
+func (k KeyStore) DoRegister(server *grpc.Server) {
+	pb.RegisterKeyStoreServiceServer(server, &k)
+}
+
+//Init a keystore
+func Init(p string) *KeyStore {
+	ks := &KeyStore{&goserver.GoServer{}, make(map[string][]byte), p}
+	ks.Register = ks
+	return ks
+}
+
+// ReportHealth alerts if we're not healthy
+func (k KeyStore) ReportHealth() bool {
+	return true
+}
 
 func main() {
-	log.Printf("Wait, what?")
+	var folder = flag.String("folder", "", "The folder to use as a base")
+	var verbose = flag.Bool("verbose", false, "Show all output")
+	flag.Parse()
+
+	server := Init(*folder)
+
+	//Turn off logging
+	if !*verbose {
+		log.SetFlags(0)
+		log.SetOutput(ioutil.Discard)
+	}
+
+	server.PrepServer()
+	server.RegisterServer("keystore", false)
+	server.Serve()
 }
