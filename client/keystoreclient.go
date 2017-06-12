8602f5532d2e4ecabd2aa2e804ee924d5d70146e
Simon Tucker <brotherlogic@gmail.com>
2017-06-11 19:52:41 -0700

    Resolved merge

--- client/keystoreclient.go
+++ client/keystoreclient.go
@@ -2,12 +2,35 @@ package keystoreclient
 
 import (
 	"context"
+	"log"
 
 	"github.com/golang/protobuf/proto"
 
 	pbd "github.com/brotherlogic/keystore/proto"
+	"github.com/brotherlogic/keystore/store"
 	google_protobuf "github.com/golang/protobuf/ptypes/any"
 )
+
+//GetTestClient gets a test client that saves to a local store
+func GetTestClient(path string) *Keystoreclient {
+	return &Keystoreclient{linker: &localLinker{s: store.Store{Mem: make(map[string][]byte), Path: path}}}
+}
+
+type localLinker struct {
+	s store.Store
+}
+
+//Save saves out a proto
+func (l *localLinker) Save(ctx context.Context, req *pbd.SaveRequest) (*pbd.Empty, error) {
+	err := l.s.LocalSaveBytes(req.Key, req.Value.Value)
+	return &pbd.Empty{}, err
+}
+
+//Read reads a proto
+func (l *localLinker) Read(ctx context.Context, req *pbd.ReadRequest) (*google_protobuf.Any, error) {
+	bytes, err := l.s.LocalReadBytes(req.Key)
+	return &google_protobuf.Any{Value: bytes}, err
+}
 
 type link interface {
 	Save(ctx context.Context, req *pbd.SaveRequest) (*pbd.Empty, error)
@@ -23,6 +46,7 @@ type Keystoreclient struct {
 // Save saves a proto
 func (c *Keystoreclient) Save(key string, message proto.Message) error {
 	bytes, _ := proto.Marshal(message)
+	log.Printf("HERE %v", c.linker)
 	_, err := c.linker.Save(context.Background(), &pbd.SaveRequest{Key: key, Value: &google_protobuf.Any{Value: bytes}})
 	return err
 }
--- client/keystoreclient_test.go
+++ client/keystoreclient_test.go
@@ -1,37 +1,14 @@
 package keystoreclient
 
 import (
-	"context"
 	"testing"
 
-	"github.com/brotherlogic/keystore/store"
-
-	pbd "github.com/brotherlogic/keystore/proto"
 	pb "github.com/brotherlogic/keystore/testproto"
-	google_protobuf "github.com/golang/protobuf/ptypes/any"
 )
-
-func getTestClient(path string) *Keystoreclient {
-	return &Keystoreclient{linker: &localLinker{s: store.Store{Mem: make(map[string][]byte), Path: path}}}
-}
-
-type localLinker struct {
-	s store.Store
-}
-
-func (l *localLinker) Save(ctx context.Context, req *pbd.SaveRequest) (*pbd.Empty, error) {
-	err := l.s.LocalSaveBytes(req.Key, req.Value.Value)
-	return &pbd.Empty{}, err
-}
-
-func (l *localLinker) Read(ctx context.Context, req *pbd.ReadRequest) (*google_protobuf.Any, error) {
-	bytes, err := l.s.LocalReadBytes(req.Key)
-	return &google_protobuf.Any{Value: bytes}, err
-}
 
 func TestSaveAndLoad(t *testing.T) {
 	tp := &pb.TestProto{Key: "Key", Value: "Value"}
-	client := getTestClient(".testsaveandload")
+	client := GetTestClient(".testsaveandload")
 	err := client.Save("/testkey", tp)
 
 	if err != nil {
