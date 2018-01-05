eb11bdaadc2b0c1faa7d26f85a31f5661e445533
Simon Tucker <brotherlogic@gmail.com>
2018-01-04 23:39:12 -0800

    Added size update to client

--- cli/cli.go
+++ cli/cli.go
@@ -15,6 +15,9 @@ import (
 	pb "github.com/brotherlogic/cardserver/card"
 	pbdi "github.com/brotherlogic/discovery/proto"
 	pbk "github.com/brotherlogic/keystore/proto"
+
+	//Needed to pull in gzip encoding init
+	_ "google.golang.org/grpc/encoding/gzip"
 )
 
 func findServer(name string) (string, int) {
@@ -61,7 +64,7 @@ func main() {
 		if port <= 0 {
 			log.Fatalf("Error in locating keystore")
 		}
-		conn, err := grpc.Dial(host+":"+strconv.Itoa(port), grpc.WithInsecure())
+		conn, err := grpc.Dial(host+":"+strconv.Itoa(port), grpc.WithInsecure(), grpc.WithMaxMsgSize(1024*1024*1024))
 		if err != nil {
 			log.Fatalf("Cannot dial master: %v", err)
 		}
@@ -76,6 +79,6 @@ func main() {
 			log.Fatalf("Error on read: %v", err)
 		}
 
-		fmt.Printf("Read %v -> %v in %v (%v)", os.Args[1], res.GetPayload(), time.Now().Sub(t), res.GetReadTime())
+		fmt.Printf("Read %v -> %v in %v (%v)", os.Args[1], len(res.GetPayload().Value), time.Now().Sub(t), res.GetReadTime())
 	}
 }
