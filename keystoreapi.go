51b3578b4622a082fc881b44d50f3ba8b65153bd
Simon Tucker <simon@brotherlogic@gmail>
2019-07-21 22:04:00 -0700

    Trying to catch errors. This closes #694

--- keystoreapi.go
+++ keystoreapi.go
@@ -283,6 +283,10 @@ func (k *KeyStore) Save(ctx context.Context, req *pb.SaveRequest) (*pb.Empty, er
 		k.coreWrites++
 	} else {
 		k.fanWrites++
+		if req.GetMeta() == nil {
+			k.RaiseIssue(ctx, "Bad fan write", fmt.Sprintf("Bad fanout request: %v", req))
+			return nil, fmt.Errorf("Bad request")
+		}
 		k.store.Meta.DeletedKeys = req.GetMeta().DeletedKeys
 	}
 
