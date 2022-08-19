94090acd1be1997b3ffacb60fb536e126826c193
Simon Tucker <brotherlogic@gmail.com>
2022-08-19 16:06:25 -0700

    Remove MOTE

--- keystore.go
+++ keystore.go
@@ -14,31 +14,6 @@ func (k *KeyStore) Shutdown(ctx context.Context) error {
 	return nil
 }
 
-//Mote promotes or demotes this server
-func (k *KeyStore) Mote(ctx context.Context, master bool) error {
-
-	if !k.mote {
-		return fmt.Errorf("Explicitly not moting, sorry")
-	}
-
-	entries := k.serverGetter.getServers()
-	for _, entry := range entries {
-		if entry.Ip != k.Registry.Ip {
-			meta := k.serverStatusGetter.getStatus(entry)
-
-			if meta.GetVersion() > k.store.Meta.GetVersion() {
-				return fmt.Errorf("We're too behind to be master (versionstore says %v, we're %v)", meta.GetVersion(), k.store.Meta.GetVersion())
-			}
-		}
-	}
-
-	if master {
-		k.state = pb.State_MASTER
-	}
-
-	return nil
-}
-
 // GetDirectory gets a directory listing
 func (k *KeyStore) GetDirectory(ctx context.Context, req *pb.GetDirectoryRequest) (*pb.GetDirectoryResponse, error) {
 	return &pb.GetDirectoryResponse{Keys: k.store.GetStored()}, nil
